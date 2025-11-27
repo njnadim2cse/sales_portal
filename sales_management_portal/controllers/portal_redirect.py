0aac895be2197009581d6085ec545fb2974d0eb7
njnadim2cse <njnadim2@gmail.com>
2025-11-27 13:08:04 +0600

    server redirect

--- sales_management_portal/controllers/portal_redirect.py
+++ sales_management_portal/controllers/portal_redirect.py
@@ -12,6 +12,20 @@ class MasterPortalRedirect(CustomerPortal):
     
     @http.route(['/my', '/my/home'], type='http', auth="user", website=True)
     def home(self, **kw):
-        """Override portal home - this should take precedence"""
+        """Override portal home - redirect to sales management dashboard"""
         _logger.info(f"MASTER REDIRECT: Portal home override for user {request.env.user.id}")
+        
+        # Check if user has access to any portal
+        user = request.env.user
+        if not (user.use_contact_portal or user.use_todo_portal or user.use_quotation_portal):
+            # If no portal access, redirect to web home
+            return request.redirect('/web')
+        
+        # Redirect to sales management dashboard
         return request.redirect('/sales_management')
+    
+    # Also override the account route to prevent conflicts
+    @http.route('/my/account', type='http', auth='user', website=True)
+    def account(self, **kw):
+        """Override account page to avoid conflicts"""
+        return super(MasterPortalRedirect, self).account(**kw)
